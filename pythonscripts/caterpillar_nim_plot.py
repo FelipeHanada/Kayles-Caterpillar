765dcc827241f8a89e5a0a61462f3f188b045c77
felipehanada <felipe.eizohanada@gmail.com>
2024-12-04 08:23:31 -0300

    fix gitignore

---
+++ pythonscripts/caterpillar_nim_plot.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+INPUT_FILE = 'caterpillar_nim_1'
+START = 0
+N = 10000
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+
+    with open(INPUT_FILE, mode='rb') as file:
+        file.seek(START * 4)
+        nimbers = np.fromfile(file, dtype=np.uint32, count=N)
+
+    ax.plot(range(START, START + len(nimbers)), nimbers)
+
+    ax.set_xlabel('n')
+    ax.set_ylabel('C(n, 1) eq nimber')
+    ax.yaxis.set_minor_locator(plt.MultipleLocator(1))
+    ax.yaxis.set_major_locator(plt.MultipleLocator(1))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(1))
+
+    plt.show()
+ 
